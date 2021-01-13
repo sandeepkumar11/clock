3afa2125c17de99b6187f08bb55c9b3385438924
Sandeep Kumar <65331437+sandeepkumar11@users.noreply.github.com>
2021-01-13 22:09:56 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+   var d = new Date();
+   var htime = d.getHours();
+   var mtime = d.getMinutes();
+   var stime = d.getSeconds();
+   var hrotation = 30*htime + mtime/2;
+   var mrotation = 6*mtime;
+   var srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
